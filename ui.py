9deb68409727eee72b78f38e855c7d971d8fa34f
kornelgatter <gatterk@gmail.com>
2019-02-21 13:55:45 +0100

    table print update

--- ui.py
+++ ui.py
@@ -2,28 +2,40 @@
 
 
 def print_table(table, title_list):
-    table.append(title_list)
-    table[-1], table[0] = table[0], table[-1]  # set the title to 0
-    lenght_list = []
-    for lines in table:
-        for items in lines:
-            lenght_list.append(items)
-    longest_words_length = len(max(lenght_list, key=len))       #look the len(max) in the new list
-    multiplier = len(title_list)*(longest_words_length-4)       # multiply it by the length_(title)list
+    table.insert(0, title_list)
+    # length_list = []
+    # for lines in table:
+    #     for items in lines:
+    #         length_list.append(items)
+    # longest_words_length = len(max(length_list, key=len))   # get the longest word in the new list
+    # multiplier = len(title_list)*(longest_words_length)   # multiply it by the number of columns
 
-    for sublist in table:
-        # print("\n")
-        print("\n", "-"*multiplier)
-        for j in sublist:
-            print("|", j, end=" "*(longest_words_length-len(j)))
-
-    print("\n","-"*multiplier)
+    # for sublist in table:
+    #    # print("\n")
+    #    print("\n", "-" * multiplier)
+    #    for j in sublist:
+    #        for subj in j:
+    #            len_of = len(subj)
+    #            longest_smallest = len_of*longest_words_length
+    #        print(f"|", f"{j:^15}", end=" "*len_of)
+    #         print(f"|{j:^}",end =" "(longest_words_length-len(j)))
+    #    print("\n", "-" * multiplier)
     # print("\n")
+    columns = list(zip(*table))
+    col_widths = [max(len(item)for item in rows) for rows in columns]
+
+    for row in table:
+        print("-"*sum(col_widths)+"-"*len(col_widths))
+        print("|".join(word.rjust(width) for word, width in zip(row, col_widths)))
+    print("-"*sum(col_widths)+"-"*len(col_widths), "\n")
+
+    # for row in table:
+    #     print("  ".join(word.rjust(width) for word, width in zip(row, col_widths)))
 
 
 def print_result(result, label):
-   # your code
-    print("\n",result)
+    # your code
+    print("\n", result)
     print(label)
 
 
